4b10949df172d46adf9cfb7eaa6d6ff91c947b7e
jesuslcorominas <jesuslcorominas@gmail.com>
2020-04-09 22:36:28 +0200

    Actualizado kotlin Agregadas librerias

--- testShared/src/main/java/com/jesuslcorominas/posts/testshared/mocks.kt
+++ testShared/src/main/java/com/jesuslcorominas/posts/testshared/mocks.kt
@@ -5,6 +5,6 @@ import com.jesuslcorominas.posts.domain.Post
 val mockedPost = Post(
     0,
     1,
-    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    "title",
+    "body"
 )
