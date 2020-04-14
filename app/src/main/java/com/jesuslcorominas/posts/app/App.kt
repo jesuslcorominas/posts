35f9e8bd42f570e77a159442d8ee7bd8184c8dc9
jesuslcorominas <jesuslcorominas@gmail.com>
2020-04-14 22:59:00 +0200

    probando fuentes

--- app/src/main/java/com/jesuslcorominas/posts/app/App.kt
+++ app/src/main/java/com/jesuslcorominas/posts/app/App.kt
@@ -1,6 +1,9 @@
 package com.jesuslcorominas.posts.app
 
 import android.app.Application
+import androidx.core.provider.FontRequest
+import androidx.emoji.text.EmojiCompat
+import androidx.emoji.text.FontRequestEmojiCompatConfig
 import com.facebook.stetho.Stetho
 import com.jesuslcorominas.posts.app.di.ApplicationComponent
 import com.jesuslcorominas.posts.app.di.ApplicationModule
@@ -25,6 +28,14 @@ class App : Application() {
         }
 
         Stetho.initializeWithDefaults(this);
-    }
 
+        val fontRequest = FontRequest(
+            "com.example.fontprovider",
+            "com.example",
+            "emoji compat Font Query",
+            R.array.com_google_android_gms_fonts_certs
+        )
+        val config = FontRequestEmojiCompatConfig(this, fontRequest)
+        EmojiCompat.init(config)
+    }
 }
